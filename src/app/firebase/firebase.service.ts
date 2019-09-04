fa227916118031c77eb4dc734f471c3f29f8e895
Cioccio <pablo.cioccio@thomsonreuters.com>
2019-09-04 18:12:02 -0300

    Removing environment configuration

--- src/app/firebase/firebase.service.ts
+++ src/app/firebase/firebase.service.ts
@@ -1,14 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
-
-  private apiUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 

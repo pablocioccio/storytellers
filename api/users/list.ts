eec0ffc9931aeb7a705eca41bbddf73b8f54f479
Cioccio <pablo.cioccio@thomsonreuters.com>
2019-09-07 11:55:38 -0300

    Lowercasing secrets

--- api/users/list.ts
+++ api/users/list.ts
@@ -4,7 +4,7 @@ import { AppMetadata, ManagementClient, User, UserMetadata } from 'auth0';
 export default async (request: NowRequest, response: NowResponse) => {
 
   const managementClient: ManagementClient = new ManagementClient({
-    clientId: `${process.env.authentication_mgmt_api_clientId}`,
+    clientId: `${process.env.authentication_mgmt_api_clientid}`,
     clientSecret: `${process.env.authentication_mgmt_api_secret}`,
     domain: `${process.env.authentication_domain}`,
   });

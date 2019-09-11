3c7536c9781804177317581a4f114a7b83179f14
Cioccio <pablo.cioccio@thomsonreuters.com>
2019-09-11 11:49:58 -0300

    Limit number of users to 5

--- src/app/games/create/create.component.ts
+++ src/app/games/create/create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/users/model/user';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-game-create',
@@ -8,18 +10,35 @@ import { User } from 'src/app/users/model/user';
 })
 export class CreateComponent implements OnInit {
 
-  users: User[] = [];
+  private MAX_USERS = 5;
+
+  users: User[] = []; // List of selected users
+  errorMessage: string; // Error message used for alerts
+  private errorSubject = new Subject<string>();
 
   constructor() { }
 
   ngOnInit() {
+    this.errorSubject.subscribe((message) => this.errorMessage = message);
+    this.errorSubject.pipe(
+      debounceTime(3000)
+    ).subscribe(() => this.errorMessage = null);
   }
 
   userSelected(user: User) {
+    // Clear any previos error messages
+    this.errorMessage = null;
+
     // Use destructuring to unpack the "id" property of User
-    if (!this.users.find(({ user_id }) => user_id === user.user_id)) {
-      this.users.push(user);
+    if (this.users.find(({ user_id }) => user_id === user.user_id)) {
+      return this.errorSubject.next(`User ${user.name} already added.`);
     }
+
+    if (this.users.length === this.MAX_USERS) {
+      return this.errorSubject.next(`Only ${this.MAX_USERS} users are allowed per game.`);
+    }
+
+    this.users.push(user);
   }
 
   removeUser(id: string) {

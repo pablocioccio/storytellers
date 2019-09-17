4710d2ceac8f9f8416bc0b33e99bff144a09fb5a
Cioccio <pablo.cioccio@thomsonreuters.com>
2019-09-17 15:32:39 -0300

    Improving search API

--- api/users/search.ts
+++ api/users/search.ts
@@ -14,10 +14,14 @@ export default async (request: NowRequest, response: NowResponse) => {
     return;
   }
 
+  const searchableFields: string[] = ['name', 'given_name', 'family_name', 'email'];
+
   const query = term.split(' ')
     .filter((word: string) => word !== '')
-    .map((word: string) => `(name:${word}* OR email:${word}*)`)
-    .join(' AND ');
+    .map((word: string) => {
+      const subQuery: string = searchableFields.map((field: string) => `${field}:${word}*`).join(' OR ');
+      return `(${subQuery})`;
+    }).join(' AND ');
 
   const managementClient: ManagementClient = new ManagementClient({
     clientId: `${process.env.authentication_mgmt_api_clientid}`,
@@ -26,7 +30,7 @@ export default async (request: NowRequest, response: NowResponse) => {
   });
 
   const users: Array<User<AppMetadata, UserMetadata>> = await managementClient.getUsers({
-    fields: 'name,email,picture',
+    fields: 'user_id,name,email,picture',
     include_fields: true,
     q: query,
     search_engine: 'v3',
